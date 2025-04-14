4ef32ac94b49cf20ea9072f3341438b7df0ae8f0
Tanmay Kulkarni <tanmayk@Tanmays-Mac-mini.local>
2025-04-14 17:34:36 +0530

    Added Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,10 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
